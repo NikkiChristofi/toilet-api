3f65e37b161e2e32e4c1e56fee9d30d57cd04fbd
Nikki Christofi <nikki.christofi@gmail.com>
2019-11-20 17:06:38 +0000

    Add uuid and new facility

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -1,5 +1,8 @@
+import uuid from 'uuid/v1'
+
 const test = [
   {
+    'id': uuid(),
     'name': 'The Magpie',
     'location': [51.5170215, -0.078297],
     'classifications': [
@@ -17,6 +20,7 @@ const test = [
     'comments': ''
   },
   {
+    'id': uuid(),
     'name': 'The Glory',
     'location': [51.5362036, -0.0770533],
     'classifications': [
@@ -28,6 +32,24 @@ const test = [
       { 'type': 'nonGendered',
         'size': 's',
         'cubicles': 3,
+        'urinals': false
+      }
+    ],
+    'comments': 'have to climb a few stairs to the toilets'
+  },
+  {
+    'id': uuid(),
+    'name': `Mother Kelly's`,
+    'location': [51.525493, -0.0822173],
+    'classifications': [
+      { 'type': 'nonGendered',
+        'size': 's',
+        'cubicles': 2,
+        'urinals': false
+      },
+      { 'type': 'nonGendered',
+        'size': 's',
+        'cubicles': 0,
         'urinals': false
       }
     ],
